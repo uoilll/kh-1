5772cf1e2411469cd499226ede49081d3c1e2693
kimjinha <jinhak94@naver.com>
2020-11-06 22:45:24 +0900

    201106

--- 04_Statement/src/com/kh/test/loop/Test5.java
+++ 04_Statement/src/com/kh/test/loop/Test5.java
@@ -23,6 +23,8 @@ public class Test5 {
 		int sHeight = 0;
 		int sWeight = 0;
 		
+		String result = "";
+		
 		int i = 1;
 		// 3명을 입력 받기 위한 while문
 		while(i <= 3) {
@@ -44,7 +46,7 @@ public class Test5 {
 			phoneNum = sc.next();
 			
 			// 입력 받은 값을 출력
-			System.out.printf("%d %s %d세  %s %dcm %dkg %s%n", i, name, age, address, height, weight, phoneNum);
+			result += i + " " + name + " " + age + "세 " + address + " " + height + "cm " + weight + "kg " + phoneNum + "\n";
 			
 			// 입력 받은 값들을 값들을 더해줌
 			sAge += age;
@@ -53,8 +55,11 @@ public class Test5 {
 			i++;
 		}
 		
+		
 		// 나이, 키, 몸무게의 평균 값을 출력
-		System.out.println("==============================");
+		System.out.println("========== 저장회원 ==========");
+		System.out.print(result);
+		System.out.println("===========================");
 		System.out.printf("평균나이 %d세 / 평균 키 : %dCM / 평균 몸무게 : %dkg", sAge/3, sHeight/3, sWeight/3);
 		
 	}
